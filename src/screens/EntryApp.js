d67ebbc369829640ebafa99c1b22a7a2b204e963
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-15 17:15:48 +0100

    tested language translation

--- src/screens/EntryApp.js
+++ src/screens/EntryApp.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import i18next from "i18next";
+import i18n from "../assets/translations/i18n";
 
 export default class EntryApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: "en",
+    };
+  }
+
+  changeLanguageIt = () => {
+    i18n.changeLanguage("it");
+    this.setState({ language: "it" });
+  };
+
+  changeLanguageEN = () => {
+    i18n.changeLanguage("en");
+    this.setState({ language: "en" });
+  };
+
   render() {
-    return <div>EntryApp</div>;
+    return (
+      <div>
+        <h1>Ciao, questo è un test</h1>
+        <p>Stiamo provando le traduzioni</p>
+        <p>{i18next.t("greeting")}</p>
+        <p>{i18next.t("test")}</p>
+        <button onClick={this.changeLanguageIt}>IT</button>
+        <button onClick={this.changeLanguageEN}>EN</button>
+      </div>
+    );
   }
 }
