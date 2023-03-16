efd090e969161a9f4e4fa9e1bb8e5d7c783a6e37
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-16 18:06:29 +0100

    created CardImg component using MUI Card and CardMedia

---
+++ src/components/functionalComponents/cardImg/CardImg.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+
+export default function MediaCard(props) {
+  return (
+    <Card sx={props.width}>
+      <CardMedia
+        component="img"
+        sx={props.height}
+        image={props.imageSrc}
+        title={props.title}
+        // style={props.style}
+      />
+    </Card>
+  );
+}
