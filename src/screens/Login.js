55cc89c768b6645ceba014a1378a928b723a67c8
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-17 15:15:50 +0100

    imported LanguageSelector to test language change from other pages

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
+import store from "../redux/store";
+// import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
+import LanguageSelector from "../components/functionalComponents/languageSelector/LanguageSelector";
 
 function Login() {
-  return <div>Login</div>;
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div>
+      Login
+      <h3>{t("test")}</h3>
+      <LanguageSelector />
+    </div>
+  );
 }
 
 export default Login;
