c616942c1aec10110d9394ce4e673f6abce7ff76
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 22:47:28 +0100

    exported columns for orders and coupons screens

--- src/utils/tableUtils.js
+++ src/utils/tableUtils.js
@@ -1,7 +1,6 @@
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import ActionsButton from "../components/functionalComponents/actionsButton/ActionsButton";
 
 export const productsListIcons = [
   {
@@ -21,6 +20,16 @@ export const productsListIcons = [
   },
 ];
 
+export const ordersColumns = [
+  { id: "id", label: "Id" },
+  { id: "date ", label: "Date" },
+  { id: "payment_status", label: "Payment Status" },
+  { id: "stauts", label: "Status" },
+  { id: "total", label: "Total" },
+  { id: "quantity", label: "Quantity" },
+  { id: "actions", label: "Actions" },
+];
+
 export const productsColumns = [
   { id: "id", label: "Id" },
   { id: "image", label: "Image" },
@@ -28,5 +37,16 @@ export const productsColumns = [
   { id: "name", label: "Name" },
   { id: "category", label: "Category" },
   { id: "starting_price", label: "Price" },
+  { id: "actions", label: "Actions" },
+];
+
+export const couponsColumns = [
+  { id: "id", label: "Id" },
+  { id: "code", label: "Code" },
+  { id: "max_usages", label: "Max Usages" },
+  { id: "expire_date", label: "Expire Date" },
+  { id: "type", label: "Type" },
+  { id: "value", label: "Value" },
+  { id: "min_order", label: "Min Order" },
   { id: "actions", label: "Actions" },
 ];
