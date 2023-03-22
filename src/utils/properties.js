8a2ce63755499d2c4823d6ac8a035b279ed6cb0d
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 10:34:41 +0100

    added products properties

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -2,3 +2,10 @@ export const PROPERTIES = {
   BASE_URL: "https://jsonplaceholder.typicode.com",
   TIMEOUT: 1000,
 };
+
+export const PRODUCTS_PROPERTIES = {
+  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com/api",
+  DETAIL_URL:
+    "http://shoesshop.eu-south-1.elasticbeanstalk.com/api/product_details",
+  TIMEOUT: 1000,
+};
