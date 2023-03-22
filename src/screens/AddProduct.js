0d77542c698128dcc1149d69c0c020e9c00d09f9
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 22:50:32 +0100

    added translations and form

--- src/screens/AddProduct.js
+++ src/screens/AddProduct.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Form from "../components/hookComponents/form/Form";
+import Header from "../components/functionalComponents/header/Header";
+import SideBar from "../components/functionalComponents/sideBar/Sidebar";
+import { useTranslation } from "react-i18next";
+import { addProductFormProps } from "../utils/formUtils";
 
 export default function AddProduct(props) {
-  // chiamata get per rpendere oggetto base product
-
-  // salvi oggetto base in const
-
-  const objFromGet = {};
+  const { t } = useTranslation();
+  const canUploadPictures = true;
 
   return (
     <div>
-      <h1>Add product</h1>
-      <Form obj={objFromGet} />
+      <Header />
+      <div style={{ display: "flex" }}>
+        <SideBar />
+        <div style={{ backgroundColor: "#f9fafb", width: "100%" }}>
+          <h1 className="screen-title">{t("addProduct")}</h1>
+          <div style={{ width: "95%", margin: "0 auto" }}>
+            <Form
+              propsData={addProductFormProps}
+              abilitatePictures={canUploadPictures}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
