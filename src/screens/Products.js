b0d652cef70d5591ba85a251f6d8e3727d7b80c1
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 10:33:23 +0100

    fetched data from API at component mount and passed data to the table via props

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -1,31 +1,41 @@
-import React, { Component } from "react";
-import withRouter from "../routing/wrapRoutingClass/withNavigation";
+import React, { useEffect, useState } from "react";
 import SideBar from "../components/functionalComponents/sideBar/Sidebar";
 import GenericTable from "../components/functionalComponents/table/GenericTable";
 import Header from "../components/functionalComponents/header/Header";
+import FiltersRow from "../components/functionalComponents/filtersRow/FiltersRow";
+import { useTranslation } from "react-i18next";
+import { getProducts } from "../services/servicesProducts";
 
-class Products extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props?.router.params.id);
-  }
-  render() {
-    return (
-      <div>
-        {/* <h1>PRODUCTS</h1>
-        <button>ADD PRODUCT</button>
-        <button>MODIFY PRODUCT</button> */}
-        <Header />
-        <div style={{ display: "flex" }}>
-          <SideBar />
-          <div style={{ backgroundColor: "red", width: "100%" }}>
-            <h1>UTENTE LOGGATO</h1>
-            <GenericTable />
+function Products(props) {
+  const [state, setState] = useState({
+    productsList: null,
+  });
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    async function getResources() {
+      const response = await getProducts();
+      console.log("RESPONSE:", response.data);
+      setState({ productsList: response.data });
+    }
+    getResources();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex" }}>
+        <SideBar />
+        <div style={{ backgroundColor: "#f9fafb", width: "100%" }}>
+          <h1 className="screen-title">{t("productsManagement")}</h1>
+          <div style={{ width: "95%", margin: "0 auto" }}>
+            <FiltersRow label="Lista prodotti" />
+            <GenericTable products={state.productsList} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withRouter(Products);
+export default Products;
