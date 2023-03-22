668d0f3b276f3bcc0da90d887577f47cd299da86
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 22:45:19 +0100

    created screen base structure

--- src/screens/Coupons.js
+++ src/screens/Coupons.js
@@ -1,24 +1,45 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import SideBar from "../components/functionalComponents/sideBar/Sidebar";
 import GenericTable from "../components/functionalComponents/table/GenericTable";
 import Header from "../components/functionalComponents/header/Header";
+import FiltersRow from "../components/functionalComponents/filtersRow/FiltersRow";
+import { useTranslation } from "react-i18next";
+import { getProducts } from "../services/servicesProducts";
+import { couponsColumns } from "../utils/tableUtils";
 
-export default class Coupons extends Component {
-  render() {
-    return (
-      <div>
-        {/* <h1>COUPON</h1>
-        <button>ADD COUPON</button>
-        <button>MODIFY COUPON</button> */}
-        <Header />
-        <div style={{ display: "flex" }}>
-          <SideBar />
-          <div style={{ backgroundColor: "red", width: "100%" }}>
-            <h1>UTENTE LOGGATO</h1>
-            <GenericTable />
+function Coupons(props) {
+  const [state, setState] = useState({
+    productsList: null,
+  });
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    async function getResources() {
+      const response = await getProducts();
+      console.log("RESPONSE:", response.data);
+      setState({ productsList: response.data });
+    }
+    getResources();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex" }}>
+        <SideBar />
+        <div style={{ backgroundColor: "#f9fafb", width: "100%" }}>
+          <h1 className="screen-title">{t("couponsManagement")}</h1>
+          <div style={{ width: "95%", margin: "0 auto" }}>
+            <FiltersRow label={t("couponsList")} />
+            <GenericTable
+              fields={state.productsList}
+              columns={couponsColumns}
+            />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Coupons;
--- src/screens/Orders.js
+++ src/screens/Orders.js
@@ -2,23 +2,41 @@ import React, { Component } from "react";
 import SideBar from "../components/functionalComponents/sideBar/Sidebar";
 import GenericTable from "../components/functionalComponents/table/GenericTable";
 import Header from "../components/functionalComponents/header/Header";
+import FiltersRow from "../components/functionalComponents/filtersRow/FiltersRow";
+import { ordersColumns } from "../utils/tableUtils";
 
-export default class Orders extends Component {
-  render() {
-    return (
-      <div>
-        {/* <h1>ORDERS</h1>
-        <button>ADD ORDER</button>
-        <button>MODIFY ORDER</button> */}
-        <Header />
-        <div style={{ display: "flex" }}>
-          <SideBar />
-          <div style={{ backgroundColor: "red", width: "100%" }}>
-            <h1>UTENTE LOGGATO</h1>
-            <GenericTable />
+import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { getOrders } from "../services/servicesOrders";
+
+export default function Orders() {
+  const [state, setState] = useState({
+    productsList: null,
+  });
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    async function getResources() {
+      const response = await getOrders();
+      console.log("RESPONSE:", response.data);
+      setState({ productsList: response.data });
+    }
+    getResources();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex" }}>
+        <SideBar />
+        <div style={{ backgroundColor: "#f9fafb", width: "100%" }}>
+          <h1 className="screen-title">Gestione ordini</h1>
+          <div style={{ width: "95%", margin: "0 auto" }}>
+            <FiltersRow label={t("ordersList")} />
+            <GenericTable fields={state.ordersList} columns={ordersColumns} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
