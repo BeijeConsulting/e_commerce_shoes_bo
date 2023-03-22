436d58369547fad8c3fe3e45500d742092b5a3af
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 22:46:57 +0100

    changed base url and added orders properties

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -1,11 +1,21 @@
 export const PROPERTIES = {
-  BASE_URL: "https://jsonplaceholder.typicode.com",
-  TIMEOUT: 1000,
+  // BASE_URL: "https://jsonplaceholder.typicode.com",
+  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com",
+  TIMEOUT: 2000,
+  API_KEY:
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXJpYW5uYUBuaWRvLml0Iiwicm9sZXMiOlsiVVNFUiIsIkFETUlOIl0sImlhdCI6MTY3OTUwNTU1MiwiZXhwIjoxNjc5NTA5MTUyfQ.MiuiwOBuim81XswRECTGYH7Qx5J1kZ2dmWHOPSv-rOI",
 };
 
 export const PRODUCTS_PROPERTIES = {
   BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com/api",
   DETAIL_URL:
     "http://shoesshop.eu-south-1.elasticbeanstalk.com/api/product_details",
+  TIMEOUT: 1000,
+};
+
+export const ORDERS_PROPERTIES = {
+  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com/orders/",
+  DETAIL_URL:
+    "http://shoesshop.eu-south-1.elasticbeanstalk.com/orders/details/",
   TIMEOUT: 1000,
 };
