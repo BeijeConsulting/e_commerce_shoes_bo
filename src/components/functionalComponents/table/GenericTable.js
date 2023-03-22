d76db5e7cdc8d030f35b3627c5d110e041dcaba2
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 10:32:07 +0100

    imported products from API

--- src/components/functionalComponents/table/GenericTable.js
+++ src/components/functionalComponents/table/GenericTable.js
@@ -7,13 +7,15 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import { exampleTable } from "../../../utils/tableUtils";
+import { productsColumns, productsListIcons } from "../../../utils/tableUtils";
 import "./genericTable.css";
+import ActionsButton from "../../functionalComponents/actionsButton/ActionsButton";
 
-function GenericTable() {
-  const { columns, rows } = exampleTable;
+function GenericTable(props) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  console.log("PROPS:", props?.products);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -25,7 +27,7 @@ function GenericTable() {
   };
 
   function mapColumns() {
-    return columns.map((column) => {
+    return productsColumns.map((column) => {
       return (
         <TableCell
           key={column.id}
@@ -39,39 +41,55 @@ function GenericTable() {
   }
 
   function mapRows() {
-    return rows
-      .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-      .map((row, key) => {
-        return (
-          <TableRow hover role="checkbox" tabIndex={-1} key={key}>
-            {columns.map((column) => {
-              const value = row[column.id];
-              return (
-                <TableCell key={column.id} align={column.align}>
-                  {column.format && typeof value === "number"
-                    ? column.format(value)
-                    : value}
-                </TableCell>
-              );
-            })}
-          </TableRow>
-        );
-      });
+    return props?.products?.map((product, key) => {
+      return (
+        <TableRow hover role="checkbox" tabIndex={-1} key={key}>
+          {productsColumns.map((column) => {
+            const value = product[column.id];
+            return (
+              <TableCell key={column.id} align={column.align}>
+                {/* {column.format && typeof value === "number"
+                  ? column.format(value)
+                  : value} */}
+                {column.id === "actions" && (
+                  <ActionsButton
+                    icons={productsListIcons}
+                    productId={product.id}
+                  />
+                )}
+
+                {column.id === "image" ? (
+                  <img
+                    src={value}
+                    alt="product"
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      borderRadius: "12px",
+                    }}
+                  />
+                ) : (
+                  value
+                )}
+              </TableCell>
+            );
+          })}
+        </TableRow>
+      );
+    });
   }
 
   return (
     <Paper
       sx={{
-        width: 650,
         overflow: "hidden",
         boxShadow: "none",
         margin: "0 auto",
+        // width: "95%",
+        backgroundColor: "transparent",
       }}
     >
-      <TableContainer
-        sx={{ height: 500, width: 650 }}
-        className="table-container"
-      >
+      <TableContainer sx={{ height: 500 }} className="table-container">
         <Table stickyHeader aria-label="sticky table" className="generic-table">
           <TableHead className="table-head">
             <TableRow>{mapColumns()}</TableRow>
@@ -82,7 +100,7 @@ function GenericTable() {
       <TablePagination
         rowsPerPageOptions={[10, 25, 50]}
         component="div"
-        count={rows.length}
+        count={props?.products?.length || 0}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
