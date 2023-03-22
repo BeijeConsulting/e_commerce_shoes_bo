57f214a388c3cfc396e178d694eba639d4ef48f1
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 22:43:11 +0100

    created generic services for orders fetching

---
+++ src/services/servicesOrders.js
@@ -0,0 +1,24 @@
+import { ORDERS_PROPERTIES, PROPERTIES } from "../utils/properties";
+
+import {
+  getData,
+  postData,
+  putData,
+  deleteData,
+} from "../genericAxios/genericAxios";
+
+async function getOrders() {
+  return await getData(ORDERS_PROPERTIES.BASE_URL + "/all", PROPERTIES.API_KEY);
+}
+
+/*async function getOrderById(id, lang) {
+  return await getData(
+    ORDERS_PROPERTIES.BASE_URL + `/details/${id}/${lang}`
+  );
+}*/
+
+async function getDetailOrder(id) {
+  return await getData(ORDERS_PROPERTIES.DETAIL_URL + `/${id}`);
+}
+
+export { getOrders, /*getOrderById,*/ getDetailOrder };
