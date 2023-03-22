a03bfab9c7f5121b67effa934e764cc559456c9e
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 10:29:15 +0100

    imported i18n

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/common.css";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import i18n from "./assets/translations/i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
