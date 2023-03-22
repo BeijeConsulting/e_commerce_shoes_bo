f440063a70ab4633969fe3ff5a024f9fc0230d3b
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 10:36:03 +0100

    removed examples and exported productsListIcons and columns

--- src/utils/tableUtils.js
+++ src/utils/tableUtils.js
@@ -1,137 +1,32 @@
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import ActionsButton from "../components/functionalComponents/actionsButton/ActionsButton";
 
-export const exampleTable = {
-  columns: [
-    { id: "id", label: "Id" },
-    { id: "date", label: "Data" },
-    { id: "paymentStatus", label: "Status pagamento" },
-    { id: "status", label: "Status" },
-    { id: "total", label: "Totale" },
-    { id: "quantity", label: "Quantità articoli" },
-    { id: "actions", label: "Actions" },
-  ],
-  rows: [
-    {
-      id: 1,
-      date: "2022-02-28",
-      paymentStatus: "Pagato",
-      status: "Consegnato",
-      total: 105.99,
-      quantity: 3,
-      actions: (
-        <>
-          <ActionsButton
-            icons={[<DeleteIcon />, <EditIcon />]}
-            labels={["Delete", "Edit"]}
-          />
-        </>
-      ),
-    },
-    {
-      id: 2,
-      date: "2022-02-26",
-      paymentStatus: "In attesa",
-      status: "In elaborazione",
-      total: 57.5,
-      quantity: 1,
-      actions: (
-        <>
-          <ActionsButton
-            icons={[<DeleteIcon />, <EditIcon />]}
-            labels={["Delete", "Edit"]}
-          />
-        </>
-      ),
-    },
-    {
-      id: 3,
-      date: "2022-02-24",
-      paymentStatus: "Non pagato",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: (
-        <>
-          <ActionsButton
-            icons={[<DeleteIcon />, <EditIcon />]}
-            labels={["Delete", "Edit"]}
-          />
-        </>
-      ),
-    },
-    {
-      id: 4,
-      date: "2022-02-22",
-      paymentStatus: "Pagamento in sospeso",
-      status: "In elaborazione",
-      total: 45.8,
-      quantity: 2,
-      actions: "Modifica",
-    },
-    {
-      id: 5,
-      date: "2022-02-20",
-      paymentStatus: "Pagamento fallito",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: "Elimina",
-    },
-    {
-      id: 6,
-      date: "2022-02-20",
-      paymentStatus: "Pagamento fallito",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: "Elimina",
-    },
-    {
-      id: 7,
-      date: "2022-02-20",
-      paymentStatus: "Pagamento fallito",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: "Elimina",
-    },
-    {
-      id: 8,
-      date: "2022-02-20",
-      paymentStatus: "Pagamento fallito",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: "Elimina",
-    },
-    {
-      id: 9,
-      date: "2022-02-20",
-      paymentStatus: "Pagamento fallito",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: "Elimina",
-    },
-    {
-      id: 10,
-      date: "2022-02-20",
-      paymentStatus: "Pagamento fallito",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: "Elimina",
-    },
-    {
-      id: 11,
-      date: "2022-02-20",
-      paymentStatus: "Pagamento fallito",
-      status: "Annullato",
-      total: 0,
-      quantity: 0,
-      actions: "Elimina",
-    },
-  ],
-};
+export const productsListIcons = [
+  {
+    icon: <VisibilityIcon />,
+    label: "View",
+    url: `/products`,
+  },
+  {
+    icon: <EditIcon />,
+    label: "Edit",
+    url: `/products/:id/edit`,
+  },
+  {
+    icon: <DeleteIcon />,
+    label: "Delete",
+    url: `/products/:id/delete`,
+  },
+];
+
+export const productsColumns = [
+  { id: "id", label: "Id" },
+  { id: "image", label: "Image" },
+  { id: "brand", label: "Brand" },
+  { id: "name", label: "Name" },
+  { id: "category", label: "Category" },
+  { id: "starting_price", label: "Price" },
+  { id: "actions", label: "Actions" },
+];
