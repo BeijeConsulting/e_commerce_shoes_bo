d69e4c8be2b2fe6f18d8e543cbaa293884fe6d07
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-22 22:48:51 +0100

    added comment for a possible way to pass bearer auth

--- src/genericAxios/genericAxios.js
+++ src/genericAxios/genericAxios.js
@@ -7,6 +7,7 @@ const axiosInstance = axios.create({
   timeout: PROPERTIES.TIMEOUT,
   headers: {
     "Content-Type": "application/json",
+    // Authorization: `Bearer ${PROPERTIES.API_KEY}`,
   },
 });
 
