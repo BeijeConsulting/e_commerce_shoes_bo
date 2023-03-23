eb651dd92b810ec5fc39bb88770068edb01864a9
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-23 17:00:43 +0100

    created AddCoupon screen

--- src/screens/AddCoupon.js
+++ src/screens/AddCoupon.js
@@ -1,5 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SideBar from "../components/functionalComponents/sideBar/Sidebar";
+import Header from "../components/functionalComponents/header/Header";
+import { useTranslation } from "react-i18next";
+import Form from "../components/hookComponents/form/Form";
+import { addCouponFormProps } from "../utils/formUtils";
+import { useParams } from "react-router-dom";
+import { getProductById } from "../services/servicesProducts";
+import MediaCard from "../components/functionalComponents/cardImg/CardImg";
 
-export default function AddCoupon() {
-  return <div>AddCoupon</div>;
+function AddCoupon(props) {
+  const { t, i18n } = useTranslation();
+  const [state, setState] = useState({
+    // product: null,
+  });
+  const language = i18n.language;
+
+  const { id } = useParams();
+
+  // useEffect(() => {
+  //   async function getResources() {
+  //     const response = await getProductById(id, language);
+  //     console.log("RESPONSE:", response.data);
+  //     setState({ ...state, product: response.data });
+  //   }
+  //   getResources();
+  // }, [language]);
+
+  return (
+    <div>
+      <Header />
+      <div style={{ display: "flex" }}>
+        <SideBar />
+        <div style={{ width: "100%" }} className="screen-bg">
+          <h1 className="screen-title">{t("addCoupon")}</h1>
+          <div
+            style={{
+              width: "95%",
+              margin: "0 auto",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: 50,
+                width: "50%",
+              }}
+            >
+              <MediaCard
+                imageSrc="https://shop.saravecchi.it/wp-content/uploads/2020/06/Coupon_NoText.jpg"
+                height={{ height: 300 }}
+                title="Coupon"
+                width={{ width: 300 }}
+                style={{
+                  boxShadow: "10px 10px 50px #0371bc",
+                  borderRadius: "25px",
+                }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "50%",
+              }}
+            >
+              <Form
+                propsData={addCouponFormProps}
+                abilitatePictures={false}
+                buttonTitle={t("add")}
+                language={language}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default AddCoupon;
