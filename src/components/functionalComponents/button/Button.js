b7f3f8389b782974cc03cc1b80359683cc878579
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-23 14:13:55 +0100

    removed unnecessary code

--- src/components/functionalComponents/button/Button.js
+++ src/components/functionalComponents/button/Button.js
@@ -1,7 +1,18 @@
 import React from "react";
+import MyButton from "@mui/material/Button";
+import "./button.css";
 
-function Button(props) {
-  return <button>{props.action}</button>;
+function Button(/*text, action*/ props) {
+  return (
+    <div className="button-container">
+      <MyButton
+        variant="contained"
+        color={props.color}
+        type={props.type} /* onClick={action}*/
+      >
+        {props.title}
+      </MyButton>
+    </div>
+  );
 }
-
-export default Button;
+export { Button };
