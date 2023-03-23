34e75690db263a78c39a17fb8420d08e4138e854
Francesco Cataletto <francesco.cataletto@beije.it>
2023-03-23 17:32:06 +0100

    created form inside addorder screen

--- src/components/hookComponents/form/Form.js
+++ src/components/hookComponents/form/Form.js
@@ -203,25 +203,25 @@ function Form(props) {
   }*/
 
   function mapProducts(products) {
-    products.map((product) => {
-      <label htmlFor={product.id}>{t(product.label)}</label>;
-      {
-        /* <br /> */
-      }
-
-      <input
-        {...register(product.name, product.errors)}
-        type={product.type}
-        id={product.id}
-        name={product.name}
-        //onChange={product.accept ? checkInputType : null}
-        className="form-input"
-      />;
+    return products.map((product) => {
+      return (
+        <div>
+          <label htmlFor={product.id}>{t(product.label)}</label>
+          {/* <br /> */}
+          <input
+            {...register(product.name, product.errors)}
+            type={product.type}
+            id={product.id}
+            name={product.name}
+            //onChange={product.accept ? checkInputType : null}
+            className="form-input"
+          />
+        </div>
+      );
     });
   }
 
   function mapAddresses(addresses) {}
-
   return (
     <div className="form">
       {
@@ -230,7 +230,7 @@ function Form(props) {
             {[...props.propsData, ...state.facultativePictures].map(
               mapFormFields
             )}
-            {props.products && mapProducts(props.products)}
+            {mapProducts(props.products)}
             {/*} {props.arrayAddresses && map()}*/}
             {/* {<input type="submit" value={props.buttonTitle}></input>} */}
             {<Button type="submit" title={props.buttonTitle} color="success" />}
