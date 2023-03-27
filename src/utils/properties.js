34de2d1b6010125a297df7ec8e1fe2c4937da82c
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-27 19:39:08 +0200

    changed API key

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -3,7 +3,7 @@ export const PROPERTIES = {
   BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com",
   TIMEOUT: 2000,
   API_KEY:
-    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbjFAZ21haWwuY29tIiwicm9sZXMiOlsiVVNFUiIsIkFETUlOIl0sImlhdCI6MTY3OTkyOTUzNiwiZXhwIjoxNjc5OTMzMTM2fQ.I5q5eMt7EELphIEERti857Fqzcm4fBpR9Xf5N6honRc",
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbjFAZ21haWwuY29tIiwicm9sZXMiOlsiVVNFUiIsIkFETUlOIl0sImlhdCI6MTY3OTkzNzIxNiwiZXhwIjoxNjc5OTQwODE2fQ.1vemrwayjLdDwlU0gkoie4L3vEMrrdiyxBsyjo74Yxg",
 };
 
 export const PRODUCTS_PROPERTIES = {
