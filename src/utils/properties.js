fb54f8b6c35ab0d1a57aabc4b5d93834e03f1286
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-28 09:29:04 +0200

    changed URL to products

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -3,13 +3,13 @@ export const PROPERTIES = {
   BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com",
   TIMEOUT: 2000,
   API_KEY:
-    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbjFAZ21haWwuY29tIiwicm9sZXMiOlsiVVNFUiIsIkFETUlOIl0sImlhdCI6MTY3OTkzNzIxNiwiZXhwIjoxNjc5OTQwODE2fQ.1vemrwayjLdDwlU0gkoie4L3vEMrrdiyxBsyjo74Yxg",
+    "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbjFAZ21haWwuY29tIiwicm9sZXMiOlsiVVNFUiIsIkFETUlOIl0sImlhdCI6MTY3OTk4ODExMywiZXhwIjoxNjc5OTg4MTczfQ.hrxo1rVEEqLKMsOs1HVbsRvBd5m5YYktc2zaWyON54Q",
 };
 
 export const PRODUCTS_PROPERTIES = {
-  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com/api",
+  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com",
   DETAIL_URL:
-    "http://shoesshop.eu-south-1.elasticbeanstalk.com/api/product_details",
+    "http://shoesshop.eu-south-1.elasticbeanstalk.com/product_details",
   TIMEOUT: 1000,
 };
 
