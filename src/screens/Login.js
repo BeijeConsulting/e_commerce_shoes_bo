3fb3d2775b852c0b6644150c5ff885e7731e0f61
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-29 12:35:05 +0200

    added user data to the global storage and set the token

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,10 +2,55 @@ import React from "react";
 import Form from "../components/hookComponents/form/Form";
 import { loginFormProps } from "../utils/formUtils";
 import { useTranslation } from "react-i18next";
+import { getLocalStorage, setLocalStorage } from "../utils/localStorageUtils";
+import { signin, getUser, getUserAuth } from "../services/servicesAuth";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUserCredentials } from "../redux/duck/user/userDuck";
+
 import "../styles/login/login.css";
 
 function Login() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onSubmit = async (data) => {
+    console.log("Success");
+    console.log(data);
+
+    const response = await signin({
+      email: data.email,
+      password: data.password,
+    });
+
+    if (response.status === 200) {
+      const user = await getUser(response.data.token);
+
+      dispatch(
+        setUserCredentials({
+          name: user.data.name,
+          surname: user.data.surname,
+          authorities: user.data.authorities,
+          email: user.data.email,
+          isLogged: true,
+        })
+      );
+
+      setLocalStorage("token", response.data.token);
+      setLocalStorage("refreshToken", response.data.refreshToken);
+
+      navigate(`/`);
+    }
+
+    console.log(response);
+
+    // setState({
+    //   ...state,
+    //   invalidEmail: false,
+    //   invalidPassword: false,
+    // });
+  };
 
   return (
     <div className="flex flex-center screen-bg w-100 login-page-container">
@@ -13,6 +58,8 @@ function Login() {
         propsData={loginFormProps}
         abilitatePictures={false}
         buttonTitle={t("login")}
+        isFromLogin={true}
+        onSubmit={onSubmit}
       />
     </div>
   );
