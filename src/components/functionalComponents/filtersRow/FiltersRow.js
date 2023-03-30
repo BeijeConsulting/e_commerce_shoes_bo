d68a16abf3aeba4bbbf1619ad497ad13201c6acb
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-30 21:08:38 +0200

    inserted AddIcon, added dynamic ADD button (url and text from props)

--- src/components/functionalComponents/filtersRow/FiltersRow.js
+++ src/components/functionalComponents/filtersRow/FiltersRow.js
@@ -1,13 +1,21 @@
 import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 import "./filtersRow.css";
 
 function filtersRow(props) {
   return (
     <div className="filtersRow-container">
       <h3>{props.label}</h3>
-      <div className="filters-container">
+      <div className="filters-container align-center">
         <h3>Searchbar</h3>
         <h3>Filtro</h3>
+        <Link to={props.addUrl}>
+          <Button variant="contained" disableElevation startIcon={<AddIcon />}>
+            Add {props.addLabel}
+          </Button>
+        </Link>
       </div>
     </div>
   );
