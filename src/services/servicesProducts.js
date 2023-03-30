13b35dc02ed48abc6b2e9787832f4dfc34a88527
Paolo Di Martino <paolo.dimartino@beije.it>
2023-03-30 13:57:10 +0200

    created function to get products with auth

--- src/services/servicesProducts.js
+++ src/services/servicesProducts.js
@@ -2,13 +2,21 @@ import { PRODUCTS_PROPERTIES } from "../utils/properties";
 
 import {
   getData,
+  getDataAuth,
   postData,
   putData,
   deleteData,
 } from "../genericAxios/genericAxios";
+import { any } from "prop-types";
 
 async function getProducts() {
   return await getData(PRODUCTS_PROPERTIES.BASE_URL + "/products");
+}
+
+async function getProductsAuth(page, perPage) {
+  return await getDataAuth(
+    PRODUCTS_PROPERTIES.BASE_URL + `/products/page=${page}/perPage=${perPage}`
+  );
 }
 
 async function getProductById(id, lang) {
@@ -21,4 +29,4 @@ async function getDetailProduct(id) {
   return await getData(PRODUCTS_PROPERTIES.DETAIL_URL + `/${id}`);
 }
 
-export { getProducts, getProductById, getDetailProduct };
+export { getProducts, getProductById, getDetailProduct, getProductsAuth };
