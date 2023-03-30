8c490b200478a23fd4874bef6e671fdd77076cee
StonesCutter <martino.manzolini@gmail.com>
2023-03-30 16:08:30 +0300

    updated servicesProducts

--- src/services/servicesProducts.js
+++ src/services/servicesProducts.js
@@ -3,9 +3,9 @@ import { PRODUCTS_PROPERTIES } from "../utils/properties";
 import {
   getData,
   getDataAuth,
-  postData,
-  putData,
-  deleteData,
+  deleteDataAuth,
+  postDataAuth,
+  putDataAuth,
 } from "../genericAxios/genericAxios";
 import { any } from "prop-types";
 
@@ -29,4 +29,32 @@ async function getDetailProduct(id) {
   return await getData(PRODUCTS_PROPERTIES.DETAIL_URL + `/${id}`);
 }
 
-export { getProducts, getProductById, getDetailProduct, getProductsAuth };
+async function addProductAuth(obj) {
+  return await postDataAuth(
+    PRODUCTS_PROPERTIES.BASE_URL + `/products/product`,
+    obj
+  );
+}
+
+async function editProductByIdAuth(id, obj) {
+  return await putDataAuth(
+    PRODUCTS_PROPERTIES.BASE_URL + `/products/product/${id}`,
+    obj
+  );
+}
+
+async function deleteProductAuthById(id) {
+  return await deleteDataAuth(
+    PRODUCTS_PROPERTIES.BASE_URL + `/products/product/${id}`
+  );
+}
+
+export {
+  getProducts,
+  getProductById,
+  getDetailProduct,
+  getProductsAuth,
+  addProductAuth,
+  editProductByIdAuth,
+  deleteProductAuthById,
+};
