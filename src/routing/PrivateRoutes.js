bc256cc8eaab5581fb5280eae0d8655dd554064d
Paolo Di Martino <paolo.dimartino@beije.it>
2023-04-04 20:36:07 +0200

    showing an access denied notification if the access to a page is forbidden

--- src/routing/PrivateRoutes.js
+++ src/routing/PrivateRoutes.js
@@ -1,13 +1,17 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 import { getLocalStorage } from "../utils/localStorageUtils";
+import { notifyAccessDenied } from "../utils/notificationsUtils";
 
 const PrivateRoutes = () => {
   // const { isLogged } = useSelector((state) => state.userDuck);
   const isLogged = getLocalStorage("isLogged");
   console.log("PRIVATEROUTES isLogged:", isLogged);
 
-  return isLogged ? <Outlet /> : <Navigate to="/login" />;
+  return isLogged ? (
+    <Outlet />
+  ) : (
+    (notifyAccessDenied(), (<Navigate to="/login" />))
+  );
 };
 
 export default PrivateRoutes;
