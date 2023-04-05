62ed5eb846242e59bb2dedd462cb3ec3027ebacf
Francesco Cataletto <francesco.cataletto@beije.it>
2023-04-05 23:43:40 +0200

    Changed URL

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -1,21 +1,19 @@
 export const PROPERTIES = {
   // BASE_URL: "https://jsonplaceholder.typicode.com",
-  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com",
+  BASE_URL: "https://shoes-api.beije.it",
   TIMEOUT: 2000,
   API_KEY:
     "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYXRhZW50cnkxQGdtYWlsLmNvbSIsInJvbGVzIjpbIlVTRVIiLCJEQVRBX0VOVFJZIl0sImlhdCI6MTY4MDI2NDU4OSwiZXhwIjoxNjgwMjY4MTg5fQ.FFNSZ1OQwAxCgbjG3QlhYFlfot4kZCQ176Y2GeSsOYo",
 };
 
 export const PRODUCTS_PROPERTIES = {
-  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com",
-  DETAIL_URL:
-    "http://shoesshop.eu-south-1.elasticbeanstalk.com/product_details",
+  BASE_URL: "https://shoes-api.beije.it",
+  DETAIL_URL: "https://shoes-api.beije.it/product_details",
   TIMEOUT: 1000,
 };
 
 export const ORDERS_PROPERTIES = {
-  BASE_URL: "http://shoesshop.eu-south-1.elasticbeanstalk.com/orders/",
-  DETAIL_URL:
-    "http://shoesshop.eu-south-1.elasticbeanstalk.com/orders/details/",
+  BASE_URL: "https://shoes-api.beije.it/orders/",
+  DETAIL_URL: "https://shoes-api.beije.it/orders/details/",
   TIMEOUT: 1000,
 };
