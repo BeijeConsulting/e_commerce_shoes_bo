534d2ede0afa6b2a350428daeec9093c83d494e6
Paolo Di Martino <paolo.dimartino@beije.it>
2023-04-05 14:55:38 +0200

    fixed screen layout

--- src/screens/CouponDetails.js
+++ src/screens/CouponDetails.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCouponByIdAuth } from "../services/servicesCoupons";
 import ViewDetails from "../components/functionalComponents/viewDetails/ViewDetails";
-import Header from "../components/functionalComponents/header/Header";
-import SideBar from "../components/functionalComponents/sideBar/Sidebar";
 function CouponDetails() {
   const [state, setState] = useState({
     coupon: null,
@@ -22,18 +20,14 @@ function CouponDetails() {
   }, []);
 
   return (
-    <div>
-      <Header />
-      <div className="flex">
-        <SideBar />
-        <div className="screen-bg w-100 flex flex-column flex-center">
-          <h1 className="screen-title">Coupon details</h1>
-          <div className="w-50">
-            {state.coupon && <ViewDetails details={state.coupon} />}
-          </div>
+    <>
+      <div className="screen-bg w-100 flex flex-column flex-center">
+        <h1 className="screen-title">Coupon details</h1>
+        <div className="w-50">
+          {state.coupon && <ViewDetails details={state.coupon} />}
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
